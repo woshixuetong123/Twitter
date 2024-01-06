857ae09bfefd17a0a9b536970fba9a1210d39319
TongXue <Tong.Xue@nordstrom.com>
2024-01-05 21:44:03 -0800

    add tweets-api

--- twitter/urls.py
+++ twitter/urls.py
@@ -17,11 +17,16 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from accounts.api import views
+# from accounts.api import views
+from accounts.api.views import UserViewSet, AccountViewSet
+from tweets.api.views import TweetViewSet
 
 router = routers.DefaultRouter()
-router.register(r'api/users', views.UserViewSet)
-router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
+# router.register(r'api/users', views.UserViewSet)
+# router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
+router.register(r'api/users', UserViewSet)
+router.register(r'api/accounts', AccountViewSet, basename='accounts')
+router.register(r'api/tweets', TweetViewSet, basename='tweets')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
