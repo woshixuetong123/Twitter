803f887f152de3941a9c97cfb8b57676b7b9ca2d
TongXue <Tong.Xue@nordstrom.com>
2024-01-07 22:14:06 -0800

    update views.py

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -1,80 +1,49 @@
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.response import Response
+from accounts.api.serializers import UserSerializer
 from friendships.models import Friendship
-from friendships.api.serializers import (
-    FollowingSerializer,
-    FollowerSerializer,
-    FriendshipSerializerForCreate,
-)
-from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
-class FriendshipViewSet(viewsets.GenericViewSet):
-    # 我们希望 POST /api/friendship/1/follow 是去 follow user_id=1 的用户
-    # 因此这里 queryset 需要是 User.objects.all()
-    # 如果是 Friendship.objects.all 的话就会出现 404 Not Found
-    # 因为 detail=True 的 actions 会默认先去调用 get_object() 也就是
-    # queryset.filter(pk=1) 查询一下这个 object 在不在
-    queryset = User.objects.all()
+class FriendshipSerializerForCreate(serializers.ModelSerializer):
+    from_user_id = serializers.IntegerField()
+    to_user_id = serializers.IntegerField()
 
-    @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
-    def followers(self, request, pk):
-        friendships = Friendship.objects.filter(to_user_id=pk).order_by('-created_at')
-        serializer = FollowerSerializer(friendships, many=True)
-        return Response(
-            {'followers': serializer.data},
-            status=status.HTTP_200_OK,
+    class Meta:
+        model = Friendship
+        fields = ('from_user_id', 'to_user_id')
+
+    def validate(self, attrs):
+        if attrs['from_user_id'] == attrs['to_user_id']:
+            raise ValidationError({
+                'message': 'from_user_id and to_user_id should be different'
+            })
+        return attrs
+
+    def create(self, validated_data):
+        from_user_id = validated_data['from_user_id']
+        to_user_id = validated_data['to_user_id']
+        return Friendship.objects.create(
+            from_user_id=from_user_id,
+            to_user_id=to_user_id,
         )
 
-    @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
-    def followings(self, request, pk):
-        friendships = Friendship.objects.filter(from_user_id=pk).order_by('-created_at')
-        serializer = FollowingSerializer(friendships, many=True)
-        return Response(
-            {'followings': serializer.data},
-            status=status.HTTP_200_OK,
-        )
 
-    @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
-    def follow(self, request, pk):
-        # 特殊判断重复 follow 的情况（比如前端猛点好多少次 follow)
-        # 静默处理，不报错，因为这类重复操作因为网络延迟的原因会比较多，没必要当做错误处理
-        if Friendship.objects.filter(from_user=request.user, to_user=pk).exists():
-            return Response({
-                'success': True,
-                'duplicate': True,
-            }, status=status.HTTP_201_CREATED)
-        serializer = FriendshipSerializerForCreate(data={
-            'from_user_id': request.user.id,
-            'to_user_id': pk,
-        })
-        if not serializer.is_valid():
-            return Response({
-                'success': False,
-                'errors': serializer.errors,
-            }, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
-        return Response({'success': True}, status=status.HTTP_201_CREATED)
+# 可以通过 source=xxx 指定去访问每个 model instance 的 xxx 方法
+# 即 model_instance.xxx 来获得数据
+# https://www.django-rest-framework.org/api-guide/serializers/#specifying-fields-explicitly
+class FollowerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(source='from_user')
+    created_at = serializers.DateTimeField()
 
-    @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
-    def unfollow(self, request, pk):
-        # 注意 pk 的类型是 str，所以要做类型转换
-        if request.user.id == int(pk):
-            return Response({
-                'success': False,
-                'message': 'You cannot unfollow yourself',
-            }, status=status.HTTP_400_BAD_REQUEST)
-        # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#delete
-        # Queryset 的 delete 操作返回两个值，一个是删了多少数据，一个是具体每种类型删了多少
-        # 为什么会出现多种类型数据的删除？因为可能因为 foreign key 设置了 cascade 出现级联
-        # 删除，也就是比如 A model 的某个属性是 B model 的 foreign key，并且设置了
-        # on_delete=models.CASCADE, 那么当 B 的某个数据被删除的时候，A 中的关联也会被删除。
-        # 所以 CASCADE 是很危险的，我们一般最好不要用，而是用 on_delete=models.SET_NULL
-        # 取而代之，这样至少可以避免误删除操作带来的多米诺效应。
-        deleted, _ = Friendship.objects.filter(
-            from_user=request.user,
-            to_user=pk,
-        ).delete()
-        return Response({'success': True, 'deleted': deleted})
+    class Meta:
+        model = Friendship
+        fields = ('user', 'created_at')
+
+
+class FollowingSerializer(serializers.ModelSerializer):
+    user = UserSerializer(source='to_user')
+    created_at = serializers.DateTimeField()
+
+    class Meta:
+        model = Friendship
+        fields = ('user', 'created_at')
