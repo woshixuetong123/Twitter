a56936d767bb86d3ad54fec8d9ff720892236e5e
TongXue <Tong.Xue@nordstrom.com>
2024-02-04 08:33:21 -0800

    add upload-photo-admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from tweets.models import Tweet, TweetPhoto
 
 from tweets.models import Tweet
 
@@ -11,3 +12,17 @@ class TweetAdmin(admin.ModelAdmin):
         'user',
         'content',
     )
+
+
+@admin.register(TweetPhoto)
+class TweetPhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        'tweet',
+        'user',
+        'file',
+        'status',
+        'has_deleted',
+        'created_at',
+    )
+    list_filter = ('status', 'has_deleted')
+    date_hierarchy = 'created_at'
