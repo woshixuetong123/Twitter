9fe3cffc6913e941c3988150c926e4ab0d1050e7
TongXue <Tong.Xue@nordstrom.com>
2024-02-17 13:34:11 -0800

    add cache-likes-count and comments-count

--- comments/listeners.py
+++ comments/listeners.py
@@ -1,4 +1,5 @@
 from utils.listeners import invalidate_object_cache
+from utils.redis_helper import RedisHelper
 
 
 def incr_comments_count(sender, instance, created, **kwargs):
@@ -9,9 +10,9 @@ def incr_comments_count(sender, instance, created, **kwargs):
         return
 
     # handle new comment
-    Tweet.objects.filter(id=instance.tweet_id)\
+    Tweet.objects.filter(id=instance.tweet_id) \
         .update(comments_count=F('comments_count') + 1)
-    invalidate_object_cache(sender=Tweet, instance=instance.tweet)
+    RedisHelper.incr_count(instance.tweet, 'comments_count')
 
 
 def decr_comments_count(sender, instance, **kwargs):
@@ -19,6 +20,6 @@ def decr_comments_count(sender, instance, **kwargs):
     from django.db.models import F
 
     # handle comment deletion
-    Tweet.objects.filter(id=instance.tweet_id)\
+    Tweet.objects.filter(id=instance.tweet_id) \
         .update(comments_count=F('comments_count') - 1)
-    invalidate_object_cache(sender=Tweet, instance=instance.tweet)
+    RedisHelper.decr_count(instance.tweet, 'comments_count')
