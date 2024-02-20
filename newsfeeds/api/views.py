20c867e2cf1d086cc5938a444e4c4dbb7ad18f4d
TongXue <Tong.Xue@nordstrom.com>
2024-02-19 23:34:58 -0800

    practice fanout

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -5,10 +5,13 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from utils.paginations import EndlessPagination
 
+
 class NewsFeedViewSet(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     pagination_class = EndlessPagination
 
+    def get_queryset(self):
+        return NewsFeed.objects.filter(user=self.request.user)
     def list(self, request):
         cached_newsfeeds = NewsFeedService.get_cached_newsfeeds(request.user.id)
         page = self.paginator.paginate_cached_list(cached_newsfeeds, request)
